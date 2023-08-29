a9bfa5f757c2bb6d0f9c455458c9fa2a9f7c00fe
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-08-29 20:56:21 +0000

    python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,12 +3,14 @@
 
 class Square:
     """This is the Square class"""
+   
     def __init__(self, size=0):
         """Initializes a Square instance.
         
         Args:
             size (int): The size of the square's sides.
         """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
