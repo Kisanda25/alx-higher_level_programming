b91bb67d5a202e506f8482da428d57db9467aea5
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-08-29 13:53:41 +0000

    Python - Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""An empty class that defines a Square"""
+"""Define a class Square."""
+
 
 class Square:
-"""An empty class called Square"""
+    """Represent a square."""
     pass
