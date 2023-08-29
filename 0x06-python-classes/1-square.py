20d1e82986d294585c7e9c86f6ecce79e13fa4ba
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-08-29 15:27:14 +0000

    Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a class Square that defines a square""""
+
+class Square:
+    """square definition"""
+    def __init__(self, size):
+    """Initialize a square
+
+    Args:
+        size(int): Private instance attribute
+    """
+    self.__size = size
