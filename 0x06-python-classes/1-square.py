fb678feefa2e2e3606b1575f70819059d9b6c86b
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-08-29 20:34:04 +0000

    python-classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,10 +4,11 @@
 
 class Square:
     """square definition"""
+
     def __init__(self, size):
         """Initialize a square
 
         Args:
             size(int): Private instance attribute
-        """
+        "
         self.__size = size
