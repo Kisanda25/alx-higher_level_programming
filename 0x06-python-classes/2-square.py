1ce7017b46b61ecb858324598bfc45dc1d87111c
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-08-29 21:13:42 +0000

    python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""Definition of class called Square"""
+"""Definition of a class Square."""
+
 
 class Square:
-    """This is the Square class"""
-   
-    def __init__(self, size=0):
-        """Initializes a Square instance.
-        
-        Args:
-            size (int): The size of the square's sides.
-        """
+    """class called square."""
 
+    def __init__(self, size=0):
+        """Initialize a Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        
         self.__size = size
