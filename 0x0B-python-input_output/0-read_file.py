e7dd739493d771f68287a12e70ad17f59d39a7d2
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-09-12 17:06:08 +0000

    Python - Input/Output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,4 @@
 def read_file(filename=""):
     """A function that reads a file""" 
     with open(filename, encoding="utf-8") as f:
-    read_file = f.read()
-    print(read_file)
+    print(f.read(), end="")
