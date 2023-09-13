8b2f8e3774c47996d13a71e695f77db6d580c2df
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-09-13 21:01:16 +0000

    Python - Inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""an empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """A base geometry."""
+    pass
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""a class Rectangle that inherits from BaseGeometry (7-base_geometry.py)"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Intializes a new Rectangle.
+
+        Args:
+            width (int): The width of Rectangle.
+            height (int): The height of then Rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
