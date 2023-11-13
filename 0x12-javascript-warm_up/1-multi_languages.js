7910037373cad5aa39407c82cbebc0f2b1619c2a
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-11-13 08:47:54 -0800

    JavaScript - Warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,6 +3,6 @@ let myVar = 'C is fun';
 let myVar2 = 'Python is cool';
 let myVar3 = 'JavaScript is amazing';
 
-console.log(myVar1);
+console.log(myVar);
 console.log(myVar2);
 console.log(myVar3);
