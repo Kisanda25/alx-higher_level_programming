63608079404424a07ee3b1ad9202da96b6db9df8
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-11-13 13:37:20 -0800

    JavaScript - Warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,13 @@
 
 if (process.argv.length <= 2)
 {
-	console.log("No argument");
+console.log("No argument");
 }
 else if (process.argv.length == 3)
 {
-	console.log("Argument found");
+console.log("Argument found");
 }
-else {
-	console.log("Arguments found");
+else 
+{
+console.log("Arguments found");
 }
