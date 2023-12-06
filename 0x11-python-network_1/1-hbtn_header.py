5644abe584de0a40179f63d8f457c8b0fa743540
Emmanuel Kisanda <emmanuelkisanda7@gmail.com>
2023-12-06 02:19:47 +0000

    Python - Network #1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL, sends a request to the URL and displays"""
+"""a Python script that takes in a URL, sends a request to the URL"""
 
 import sys
 import urllib.request
